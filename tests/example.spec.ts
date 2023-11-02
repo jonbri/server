6ee71dcbb9289e082a87f60c91a0b2a1f5811cb0
Jonathan Brink <jonathandavidbrink@gmail.com>
2023-11-02 10:23:46 -0400

    wip

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('http://localhost:4000/');
-  await expect(page).toHaveTitle(/localhost:4000/);
+  await expect(page).toHaveTitle(/Testing/);
 });
 
 // test('get started link', async ({ page }) => {
